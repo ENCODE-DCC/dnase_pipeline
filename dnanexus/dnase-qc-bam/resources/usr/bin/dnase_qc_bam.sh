a8f8ae728b2d12cea80c1b3b1b8271574019ce5d
tdreszer <tdreszer@yahoo.com>
2017-04-21 19:04:51 +0000

    Added dnase-qc-bam which combines eval-bam and qc-hotspot1 for the next version of the pipeline.

---
+++ dnanexus/dnase-qc-bam/resources/usr/bin/dnase_qc_bam.sh
@@ -0,0 +1,129 @@
+#!/bin/bash -e
+
+if [ $# -ne 8 ]; then
+    echo "usage v1: dnase_qc_bam.sh <filtered.bam> <sample_size> <ncpus> <pe_or_se> <assembly> <mappable.tgz> <read_length> <hotspot_dir>"
+    echo "Evaluates filtered aligned reads for DNase using HotSpot1 and other tools.  Is independent of DX and encodeD."
+    echo "Requires edwBamFilter, edwBamStats, samtools, Rscript, phantompeakqualtools, caTools, snow, spp, gawk,"
+    echo "         hotspot, hotspot.py (GCAP), bedops and bedtools on path."
+    exit -1; 
+fi
+filtered_bam=$1 # filtered bam file.
+sample_size=$2  # number of sample reads to evaluate (e.g. 15000000)
+ncpus=$3        # Number of cpus available
+pe_or_se=$4     # Either "pe" for paired-end or "se" for single-end
+assembly=$5     # Genome assembly matching chrom.sizes and bam.
+mappable_tgz=$6 # Archive with mappable_target.starch, center_sites.starch and chrom_sizes.bed created by dnase_index.bwa.sh.
+read_length=$7  # Length of reads to match hotspot Kmer size (must be 32, 36, 40, 50, 58, 72, 76, or 100).
+hotspot_dir=$8  # Directory where hotpot is installed 
+if [ "$pe_or_se" != "pe" ] && [ "$pe_or_se" != "se" ]; then
+    echo "-- ERROR: Declare either 'pe' for paied-end or 'se' for single-end."
+    exit 1
+fi
+if [ "$read_length" != "32" ] && [ "$read_length" != "36" ] && [ "$read_length" != "40" ] && [ "$read_length" != "50" ] \
+&& [ "$read_length" != "58" ] && [ "$read_length" != "72" ] && [ "$read_length" != "76" ] && [ "$read_length" != "100" ]; then
+    echo "* ERROR: Read length ($read_length) must be one of 32, 36, 40, 50, 58, 72, 76, or 100."
+    exit 1
+fi
+if [ "$assembly" == "GRCh38" ]; then
+    # HotSpot1 uses UCSC assembly names
+    assembly="hg38"
+fi 
+
+bam_input_root=${filtered_bam%.bam}
+bam_no_chrM_root="${bam_input_root}_no_chrM"
+bam_sample_root="${bam_input_root}_${sample_size}_sample"
+echo "-- Sampled alignments file will be: '${bam_sample_root}.bam'"
+
+echo "-- Filter out chrM..."
+set -x
+edwBamFilter -sponge -chrom=chrM $filtered_bam ${bam_no_chrM_root}.bam  ## qc based on bam without chrm
+set +x
+if [ "$pe_or_se" == "pe" ]; then
+    echo "-- Sorting by name for paired-end data..."
+    # Note the sort by name which is needed for proper pe sampling
+    set -x
+    samtools sort -@ $ncpus -m 6G -n -f ${bam_no_chrM_root}.bam ${bam_no_chrM_root}_byname.sam ## for pbc usage
+    samtools view -hb ${bam_no_chrM_root}_byname.sam > ${bam_no_chrM_root}_byname.bam
+    rm *.sam
+    set +x
+    bam_no_chrM_root="${bam_no_chrM_root}_byname"
+fi
+set -x
+samtools index ${bam_no_chrM_root}.bam
+set +x
+
+echo "-- Generating stats on $sample_size reads..."
+set -x
+edwBamStats -sampleBamSize=$sample_size -u4mSize=$sample_size -sampleBam=${bam_sample_root}.bam \
+                      ${bam_no_chrM_root}.bam ${bam_no_chrM_root}_sampling_edwBamStats.txt
+edwBamStats ${bam_sample_root}.bam ${bam_sample_root}_edwBamStats.txt
+samtools index ${bam_sample_root}.bam
+set +x
+
+echo "-- Running spp..."
+set -x
+# awk didn't work, so use gawk and pre-create the tagAlign 
+samtools view -F 0x0204 -o - ${bam_sample_root}.bam | \
+   gawk 'BEGIN{OFS="\t"}{if (and($2,16) > 0) {print $3,($4-1),($4-1+length($10)),"N","1000","-"} else {print $3,($4-1),($4-1+length($10)),"N","1000","+"} }' \
+   | gzip -c > ${bam_sample_root}.tagAlign.gz
+Rscript phantompeakqualtools/run_spp.R -x=-500:-1 -s=-500:5:1500 -rf -c=${bam_sample_root}.tagAlign.gz \
+                                    -out=${bam_sample_root}_spp.txt > ${bam_sample_root}_spp_out.txt
+touch ${bam_sample_root}_spp.txt
+set +x
+
+echo "-- Running pbc..."
+# Seth interprets:
+# TotalReadPairs \t DistinctReadPairs \t OneReadPair \t TwoReadPairs \t NRF=Distinct/Total \t PBC1=OnePair/Distinct \t PBC2=OnePair/TwoPair
+# Tim reinterprets:
+# Sampled Reads \t Distinct Locations Mapped \t Single-read Locations \t Multi-read Locations \t NRF (Non-Redundant Fraction)=Distinct Locations/Sample Reads \t PBC1=Single-read Locations/Distinct Locations \t PBC2=Single-read Locations/Multi-read Locations
+set -x
+bedtools bamtobed -i ${bam_sample_root}.bam | awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$6}' | grep -v 'chrM' | sort | uniq -c \
+    | awk 'BEGIN{mt=0;m0=0;m1=0;m2=0} ($1==1){m1=m1+1} ($1==2){m2=m2+1} {m0=m0+1} {mt=mt+$1} END{printf "%d\t%d\t%d\t%d\t%f\t%f\t%f\n",mt,m0,m1,m2,m0/mt,m1/m0,m1/m2}' \
+    > ${bam_sample_root}_pbc.txt
+set +x
+
+echo "-- Opening archive of mappable regions..."
+set -x
+tar -xzf $mappable_tgz
+set +x
+# Expect: chrom_sizes.bed, center_sites.starch, mappable_target.starch, GRCh38_no_alts.K36.mappable_only.starch, GRCh38.blacklist.bed
+
+echo "-- Creating chromInfo.bed from chrom_sizes.bed..."
+# Reducing to min chroms appears to be necessary for hotspot1
+grep -w chr[1-9] chrom_sizes.bed > min_chrom.sizes
+grep -w chr[1-2][0-9] chrom_sizes.bed >> min_chrom.sizes
+grep -w chr[X,Y] chrom_sizes.bed >> min_chrom.sizes
+## sort-bed is important!
+cat min_chrom.sizes | awk '{printf "%s\t%s\t%s\t%s\n",$1,$2,$3,$1}' | sort-bed - > ${assembly}.chromInfo.bed
+
+echo "-- Moving hotspot1 mappable file to expected name and place..."
+mappable=${assembly}.K${read_length}.mappable_only
+satellites=Satellite.${assembly}
+if [ "$read_length" == "36" ]; then
+    set -x
+    unstarch mappable_target.starch > ${hotspot_dir}/hotspot-distr/data/${mappable}.bed
+    # Note that the blacklist is already removed from mappable_targets so will be a noop but hotspot1 doesn't know that.
+    cp *.blacklist.bed ${hotspot_dir}/hotspot-distr/data/${satellites}.bed
+    set +x
+else
+    # Not a good idea: probably only hg19 will work
+    set -x
+    wget http://www.uwencode.org/proj/hotspot/${mappable}.bed -O ${hotspot_dir}/hotspot-distr/data/${mappable}.bed
+    wget http://www.uwencode.org/proj/hotspot/${satellites}.bed -O ${hotspot_dir}/hotspot-distr/data/${satellites}.bed
+    set +x
+fi
+
+echo "-- Running hotspot.py..."
+set -x
+mkdir tmp
+mkdir out
+cp ${assembly}.chromInfo.bed ${hotspot_dir}/hotspot-distr/data/
+hotspot.py -o ${hotspot_dir}/hotspot-distr/ ${bam_sample_root}.bam $assembly DNase-seq $read_length tmp out
+
+cp tmp/${bam_sample_root}.spot.out ${bam_sample_root}_hotspot1_qc.txt
+set +x
+
+echo "-- The results..."
+ls -l ${bam_sample_root}*
+df -k .
+
---
+++ dnanexus/dnase-qc-bam/src/dnase-qc-bam.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+# dnase-eval-bam.sh - Evaluates sample of bam for the ENCODE DNase-seq pipeline.
+
+main() {
+    echo "* Installing phantompeakqualtools, caTools, snow and spp..." 2>&1 | tee -a install.log
+    set -x
+    # phantompeakqualtools  : also resquires boost C libraries (on aws), boost C libraries (on aws) samtools (in resources/usr/bin)
+    wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantompeakqualtools/ccQualityControl.v.1.1.tar.gz -O phantompeakqualtools.tgz | tee -a install.log 2>&1
+    mkdir phantompeakqualtools
+    #tar -xzf phantompeakqualtools.tgz -C phantompeakqualtools --strip-components=1
+    tar -xzf phantompeakqualtools.tgz
+    cd phantompeakqualtools
+    # By not having caTools and snow in execDepends, we can at least show which versions are used
+    echo "install.packages(c('caTools','snow'),dependencies=TRUE,repos='http://cran.cnr.berkeley.edu/')" > installPkgs.R
+    echo "install.packages('spp_1.10.1.tar.gz')" >> installPkgs.R
+    cat installPkgs.R
+    sudo Rscript installPkgs.R >> install.log 2>&1
+    grep DONE install.log
+    set +x
+    done_count=`grep -c DONE install.log`
+    if [ "$done_count" != "6" ]; then
+        set -x
+        cat install.log
+        set -x
+    fi
+    cd ..
+    exe_dir="`pwd`"
+    wget ftp://ftp.gnu.org/gnu/gsl/gsl-latest.tar.gz -O gsl.tgz >> install.log 2>&1
+    mkdir gsl
+    tar -xzf gsl.tgz -C gsl --strip-components=1
+    cd gsl
+    ./configure >> install.log 2>&1
+    make > install.log 2>&1
+    sudo make install >> install.log 2>&1
+    gsl-config --libs > install.log 2>&1
+    export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+    cd ..
+    wget https://github.com/rthurman/hotspot/archive/v4.1.0.tar.gz -O hotspot.tgz >> install.log 2>&1
+    mkdir hotspot
+    tar -xzf hotspot.tgz -C hotspot --strip-components=1
+    cd hotspot/hotspot-distr/hotspot-deploy
+    make >> install.log 2>&1
+    # Can either put bin in path or copy contents of bin to /usr/bin
+    export PATH=${exe_dir}/hotspot/hotspot-distr/hotspot-deploy/bin:${PATH}
+    cd ../../../
+    set +x; 
+    # additional executables in resources/usr/bin
+    
+    # additional executables in resources/usr/bin
+    set +x
+    
+    # If available, will print tool versions to stderr and json string to stdout
+    versions=''
+    if [ -f /usr/bin/tool_versions.py ]; then 
+        versions=`tool_versions.py --dxjson dnanexus-executable.json`
+    fi
+ 
+    echo "* Value of bam_filtered:     '$bam_filtered'"
+    echo "* Value of pe_or_se:         '$pe_or_se'"
+    echo "* Value of sample_size:      '$sample_size'"
+    echo "* Value of hotspot_mappable: '$hotspot_mappable'"
+    #echo "* Value of read_length:     '$read_length'"
+    echo "* Value of genome:           '$genome'"
+    echo "* Value of nthreads:         '$nthreads'"
+
+    echo "* Download files..."
+    # expecting *_bwa_biorep_filtered.bam
+    bam_input_root=`dx describe "$bam_filtered" --name`
+    # Better to leave the whole suffix!
+    bam_input_root=${bam_input_root%.bam}
+    dx download "$bam_filtered" -o ${bam_input_root}.bam
+    echo "* bam file: '${bam_input_root}.bam'"
+    
+    #dx download "$chrom_sizes" -o chrom.sizes
+    mappable_archive=`dx describe "$hotspot_mappable" --name`
+    dx download "$hotspot_mappable"
+
+    # check read_length
+    read_length=36   # Only supporting read_length 36 at this time
+    #if [ -f /usr/bin/parse_property.py ]; then 
+    #    read_len=`parse_property.py -f "$bam_file" -p "read_length" --quiet`
+    #    if [ "$read_len" != "" ] && [ "$read_length" -ne "$read_len" ]; then
+    #        if [ "$read_len" == "32" ] || [ "$read_len" == "36" ] || [ "$read_len" == "40" ] || [ "$read_len" == "50" ] \
+    #        || [ "$read_len" == "58" ] || [ "$read_len" == "72" ] || [ "$read_len" == "76" ] || [ "$read_len" == "100" ]; then
+    #            echo "* NOTE: Read length ($read_length) does not match discovered read size ($read_len). Using $read_len."
+    #            read_length=$read_len
+    #        else
+    #            echo "* WARNING: Read length ($read_length) does not match discovered read size ($read_len)."
+    #        fi
+    #    fi
+    #fi
+
+    echo "* ===== Calling DNAnexus and ENCODE independent script... ====="
+    set -x
+    dnase_qc_bam.sh ${bam_input_root}.bam $sample_size $nthreads $pe_or_se $genome $mappable_archive $read_length hotspot/
+    set +x
+    echo "* ===== Returned from dnanexus and encodeD independent script ====="
+    bam_sample_root="${bam_input_root}_${sample_size}_sample"
+
+    echo "* Prepare metadata for sampled bam..."
+    qc_sampled=''
+    reads_sampled=$sample_size
+    if [ -f /usr/bin/qc_metrics.py ]; then
+        qc_sampled=`qc_metrics.py -n edwBamStats -f ${bam_sample_root}_edwBamStats.txt`
+        reads_sampled=`qc_metrics.py -n edwBamStats -f ${bam_sample_root}_edwBamStats.txt -k readCount`
+        # Note that all values in ${bam_sample_root}_spp.txt are found in ${bam_sample_root}_spp_out.txt
+        meta=`qc_metrics.py -n phantompeaktools_spp -f ${bam_sample_root}_spp_out.txt`
+        qc_sampled=`echo $qc_sampled, $meta`
+        meta=`qc_metrics.py -n pbc -f ${bam_sample_root}_pbc.txt`
+        qc_sampled=`echo $qc_sampled, $meta`
+        meta=`qc_metrics.py -n pbc_spp --string "{ $qc_sampled }"`
+        qc_sampled=`echo $qc_sampled, $meta`
+        meta=`qc_metrics.py -n hotspot -f ${bam_sample_root}_hotspot1_qc.txt`
+        qc_sampled=`echo $qc_sampled, $meta`
+    fi
+    # All qc to one file per target file:
+    echo "===== edwBamStats ====="           > ${bam_sample_root}_qc.txt
+    cat ${bam_sample_root}_edwBamStats.txt  >> ${bam_sample_root}_qc.txt
+    echo " "                                >> ${bam_sample_root}_qc.txt
+    echo "===== phantompeaktools spp =====" >> ${bam_sample_root}_qc.txt
+    cat ${bam_sample_root}_spp_out.txt      >> ${bam_sample_root}_qc.txt
+    echo " "                                >> ${bam_sample_root}_qc.txt
+    echo "===== bedtools pbc  ====="        >> ${bam_sample_root}_qc.txt
+    echo -e "Sampled Reads\tDistinct Locations Mapped\tSingle-read Locations\tMulti-read Locations\tNRF (Non-Redundant Fraction)=Distinct Locations/Sample Reads\tPBC1=Single-read Locations/Distinct Locations\tPBC2=Single-read Locations/Multi-read Locations" >> ${bam_sample_root}_qc.txt
+    cat ${bam_sample_root}_pbc.txt          >> ${bam_sample_root}_qc.txt
+    echo "===== HotSpot1 ====="             >> ${bam_sample_root}_qc.txt
+    cat ${bam_sample_root}_hotspot1_qc.txt  >> ${bam_sample_root}_qc.txt
+        
+    echo "* Upload results..."
+    bam_sample=$(dx upload ${bam_sample_root}.bam --details "{ $qc_sampled }" --property SW="$versions" \
+                            --property pe_or_se=$pe_or_se --property sampled_reads="$reads_sampled" \
+                            --property read_length="$read_len" --brief)
+    bam_sample_qc=$(dx upload ${bam_sample_root}_qc.txt   --details "{ $qc_sampled }" --property SW="$versions" --brief)
+
+    dx-jobutil-add-output bam_sample "$bam_sample" --class=file
+    dx-jobutil-add-output bam_sample_qc "$bam_sample_qc" --class=file
+
+    dx-jobutil-add-output sampled_reads "$reads_sampled" --class=string
+    dx-jobutil-add-output metadata "{ $qc_sampled }" --class=string
+
+    echo "* Finished."
+}
