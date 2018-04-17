196d9117f247164293abd1af04f650f26c2b9e56
Tim Dreszer <tdreszer@stanford.org>
2018-04-17 14:12:55 -0700

    required indexing bam before filtering down to 22_XY

--- dnanexus/dnase-call-hotspots/resources/usr/bin/dnase_hotspot.sh
+++ dnanexus/dnase-call-hotspots/resources/usr/bin/dnase_hotspot.sh
@@ -27,6 +27,7 @@ set +x
 
 echo "-- Filtering bam to only 22+XY..."
 set -x
+samtools index $bam_file
 samtools view -b $bam_file chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > chrs_22_XY.bam
 set +x
 
